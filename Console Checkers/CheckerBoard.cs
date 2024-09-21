0843b897f4e2e402d202564c984d19d4b65954b6
James.Edwards <James.Edwards@GMR-118-2-2>
2024-09-21 16:00:39 -0700

    hehehaw

--- Console Checkers/CheckerBoard.cs
+++ Console Checkers/CheckerBoard.cs
@@ -13,126 +13,46 @@ namespace Console_Checkers
     {
         None = 0,
 
-        Exists = 1,
-        Red = 2,
+        HasChecker = 1,
+        Player = 2,
         King = 4,
 
-        BlackPawn = Exists,
-        RedPawn = Exists | Red,
+        ComPawn = HasChecker,
+        PlayerPawn = HasChecker | Player,
 
-        BlackKing = BlackPawn | King,
-        RedKing = RedPawn | King
+        ComKing = ComPawn | King,
+        PlayerKing = PlayerPawn | King
     }
-    /*
+    
     internal class CheckerBoard
     {
-
         public int[] Rows = new int[8]; //each 3 bits is a peice First: 0 is empty, 1 is not. Second: 0 is Black, 1 is Red, Third: 0 is Normal, 1 is King
         public CheckerBoard(int[] rows)
         {
             Rows = rows;
         }
-        public List<int[]> GetNextMoves(int[] Board, bool RedTurn)
+        public List<int[]> GetNextMoves(int[] Board, bool PlayerTurn)
         {
             List<(int[], bool Jumped)> PossibleBoardStates = new();
             bool MustJump = false;
             for (int i = 0; i < Board.Length; i++)
             {
-                for (int j = 0; j < 24; j += 3)
+                for (int j = 0; j < 9; j += 3)
                 {
-                    int desiredBits = Board[i] >> j;
-                    TileType currentTile = (TileType)desiredBits;
-                    
-                    if (currentTile.HasFlag(TileType.None))
+                    if (j == 0) //not the leftmost
                     {
-                        continue;
-                    }
-                    if(currentTile.HasFlag(TileType.Red)) //if it is red
-                    {
-                        if (!RedTurn)
+                        if (!MustJump)
                         {
-                            continue;
-                        }
-                        if(i != 0) //if it can move upward
-                        {
-                            if(j != 0) //if it is not the leftmost
-                            {
-                                TileType NextTile = (TileType)(Board[i-1] >> (j-3));
-                                if(NextTile.HasFlag(TileType.None))
-                                {
-                                    if (!MustJump)
-                                    {
-                                        int[] TempBoard = Board.ToArray();
-                                        TempBoard[i] = TempBoard[i] & ~(7 >> j); //changing current spot to empty
-                                        TempBoard[i - 1] = TempBoard[i - 1] | ((int)NextTile >> (j - 3)); //setting 
-                                        PossibleBoardStates.Add((TempBoard, false));
-                                    }
-                                }
-                                else if (!NextTile.HasFlag(TileType.Red))
-                                {
-                                    
-                                }
-                            }
-                            if (j != 21) //if it is not the rightmost
-                            {
-                                TileType NextTile = (TileType)(Board[i - 1] >> (j + 3));
-                                if (NextTile.HasFlag(TileType.None))
-                                {
-                                    if (!MustJump)
-                                    {
-                                        int[] TempBoard = Board.ToArray();
-                                        TempBoard[i] = TempBoard[i] & ~(7 >> j);
-                                        TempBoard[i - 1] = TempBoard[i - 1] | ((int)NextTile >> (j + 3));
-                                        PossibleBoardStates.Add((TempBoard, false));
-                                    }
-                                }
-                                else if (!NextTile.HasFlag(TileType.Red))
-                                {
 
-                                }
-                            }
-                        }
-                        if(currentTile.HasFlag(TileType.King)) //if it can move downward
-                        {
-                            if (j != 0) //if it is not the leftmost
-                            {
-                                TileType NextTile = (TileType)(Board[i + 1] >> (j - 3));
-                                if (NextTile.HasFlag(TileType.None))
-                                {
-                                    if (!MustJump)
-                                    {
-                                        int[] TempBoard = Board.ToArray();
-                                        TempBoard[i] = TempBoard[i] & ~(7 >> j);
-                                        TempBoard[i + 1] = TempBoard[i + 1] | ((int)NextTile >> (j - 3));
-                                        PossibleBoardStates.Add((TempBoard, false));
-                                    }
-                                }
-                                else if (!NextTile.HasFlag(TileType.Red))
-                                {
-
-                                }
-                            }
-                            if (j != 21) //if it is not the rightmost
-                            {
-                                TileType NextTile = (TileType)(Board[i + 1] >> (j + 3));
-                                if (NextTile.HasFlag(TileType.None))
-                                {
-                                    if (!MustJump)
-                                    {
-                                        int[] TempBoard = Board.ToArray();
-                                        TempBoard[i] = TempBoard[i] & ~(7 >> j);
-                                        TempBoard[i + 1] = TempBoard[i + 1] | ((int)NextTile >> (j + 3));
-                                        PossibleBoardStates.Add((TempBoard,false));
-                                    }
-                                }
-                                else if (!NextTile.HasFlag(TileType.Red))
-                                {
-
-                                }
-                            }
                         }
                     }
+                    if (j == 9) //not the rightmos
+                    {
+                        if (!MustJump)
+                        {
 
+                        }
+                    }
                 }
             }
 
@@ -142,7 +62,7 @@ namespace Console_Checkers
         public List<int[]> RecursiveJumping(int[] Board, Point CurrPos, TileType Curr)
         {
             //setting what opposing checker it will look for
-            TileType Opp = TileType.Red;
+            TileType Opp = TileType.Player;
             if (Curr.HasFlag(TileType.Red))
             {
                 Opp = TileType.Exists;
@@ -179,5 +99,5 @@ namespace Console_Checkers
 
 
     }
-    */
+   
 }
--- Console Checkers/Program.cs
+++ Console Checkers/Program.cs
@@ -11,8 +11,13 @@ class Program
         Console.BackgroundColor = temp;
     }
 
-    public static void VisualizeGame(short[] GameState) //top left and other red tiles are the empty ones
+    public static void VisualizeGame(ushort[] GameState) //top left and other red tiles are the empty ones
     {
+        ConsoleColor PlayerColor = ConsoleColor.Red;
+        ConsoleColor ComColor = ConsoleColor.Blue;
+        ConsoleColor TileColorOne = ConsoleColor.Black;
+        ConsoleColor TileColorTwo = ConsoleColor.White;
+        ConsoleColor KingColor = ConsoleColor.Gray;
         const int TileSize = 6;
         const int HalfTheAmountOfTiles = 4;
         Console.Clear();
@@ -34,16 +39,17 @@ class Program
                 {
                     Console.BackgroundColor = ConsoleColor.Gray;
                     Console.Write("  "); //left side of border
+                    #region Top Or Bottom of Checkerboard
                     if (j == 0 || j == TileSize - 1) //bottom or top of the square
                     {
                         for (int k = 0; k < HalfTheAmountOfTiles; k++)
                         {
-                            Console.BackgroundColor = ConsoleColor.DarkRed;
+                            Console.BackgroundColor = TileColorOne;
                             for (int l = 0; l < TileSize; l++)
                             {
                                 Console.Write("  ");
                             }
-                            Console.BackgroundColor = ConsoleColor.White;
+                            Console.BackgroundColor = TileColorTwo;
                             for (int l = 0; l < TileSize; l++)
                             {
                                 Console.Write("  ");
@@ -51,57 +57,69 @@ class Program
 
                         }
                     }
+                    #endregion
                     else
                     {
+                        #region Middle Sections
                         for (int k = 0; k < HalfTheAmountOfTiles; k++)
                         {
-                            Console.BackgroundColor = ConsoleColor.DarkRed;
+                            Console.BackgroundColor = TileColorOne;
                             Console.Write("  ");
                             bool Middle = false;
-                            if ((TileSize % 2 == 0 && (j == TileSize / 2 || j == (TileSize / 2) + 1)) || (TileSize % 2 == 1 && j == (TileSize / 2) + 1)) //middle tiles
+                            if ((TileSize % 2 == 0 && (j == TileSize / 2 || j == (TileSize / 2) - 1)) || (TileSize % 2 == 1 && j == (TileSize / 2) + 1)) //middle tiles
                             {
                                 Middle = true;
                             }
-                            if (!Middle)
-                            {
-                                Console.Write("  ");
-                            }
-                            #region TheChecker
                             TileType Curr = (TileType)((GameState[i] >> (k * 3)) & 7);
-                            if (Curr.HasFlag(TileType.Exists))
+                            //Edges of the piece
+                            if (Middle)
                             {
-                                if (Curr.HasFlag(TileType.Red))
+                                if (Curr.HasFlag(TileType.HasChecker))
                                 {
-                                    Console.BackgroundColor = ConsoleColor.Red;
+                                    if (Curr.HasFlag(TileType.Player))
+                                    {
+                                        Console.BackgroundColor = PlayerColor;
+                                    }
+                                    else
+                                    {
+                                        Console.BackgroundColor = ComColor;
+                                    }
+                                }
+                            }
+                            ConsoleColor SavedColor = Console.BackgroundColor;
+                            Console.Write("  ");
+                            
+                            if (Curr.HasFlag(TileType.HasChecker))
+                            {
+                                if (Curr.HasFlag(TileType.Player))
+                                {
+                                    Console.BackgroundColor = PlayerColor;
                                 }
                                 else
                                 {
-                                    Console.BackgroundColor = ConsoleColor.Black;
+                                    Console.BackgroundColor = ComColor;
                                 }
-                            }
-                            if (Middle)
-                            {
-                                for (int l = 0; l < TileSize - 2; l++)
+                                if (Curr.HasFlag(TileType.King) && Middle)
                                 {
-                                    Console.Write("  ");
+                                    Console.BackgroundColor = KingColor;
                                 }
                             }
-                            else
-                            {
-                                for (int l = 0; l < TileSize - 4; l++)
-                                {
-                                    Console.Write("  ");
-                                }
-                            }
-                            #endregion
-                            Console.BackgroundColor = ConsoleColor.DarkRed;
+                            Console.Write("    ");
+                            Console.BackgroundColor = SavedColor;
                             Console.Write("  ");
-                            if (!Middle)
+                            Console.BackgroundColor = TileColorOne;
+                            Console.Write("  ");
+                            #region White Sqaures
+                            Console.BackgroundColor = TileColorTwo;
+                            for (int l = 0; l < TileSize; l++)
                             {
                                 Console.Write("  ");
                             }
+                            #endregion
                         }
+                        #endregion
                     }
+                    //right border
                     Console.BackgroundColor = ConsoleColor.Gray;
                     Console.Write("  ");
                     WorkingWriteLine();
@@ -109,12 +127,88 @@ class Program
             }
             else
             {
-                WorkingWriteLine();
-                WorkingWriteLine();
-                WorkingWriteLine();
-                WorkingWriteLine();
-                WorkingWriteLine();
-                WorkingWriteLine();
+                for (int j = 0; j < TileSize; j++)
+                {
+                    //left side of border
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                    Console.Write("  ");
+                    for (int k = 0; k < HalfTheAmountOfTiles; k++)
+                    {
+                        #region White Tiles
+                        Console.BackgroundColor = TileColorTwo;
+                        for (int l = 0; l < TileSize; l++)
+                        {
+                            Console.Write("  ");
+                        }
+                        #endregion
+                        if (j == 0 || j == TileSize - 1) //if top or bottom of tile
+                        {
+                            Console.BackgroundColor = TileColorOne;
+                            for (int l = 0; l < TileSize; l++)
+                            {
+                                Console.Write("  ");
+                            }
+                        }
+                        else
+                        {
+                            #region MiddleSections
+
+                            Console.BackgroundColor = TileColorOne;
+                            Console.Write("  ");
+                            bool Middle = false;
+                            if ((TileSize % 2 == 0 && (j == TileSize / 2 || j == (TileSize / 2) - 1)) || (TileSize % 2 == 1 && j == (TileSize / 2) + 1)) //middle tiles
+                            {
+                                Middle = true;
+                            }
+                            TileType Curr = (TileType)((GameState[i] >> (k * 3)) & 7);
+                            //Edges of the piece
+                            if (Middle)
+                            {
+                                if (Curr.HasFlag(TileType.HasChecker))
+                                {
+                                    if (Curr.HasFlag(TileType.Player))
+                                    {
+                                        Console.BackgroundColor = PlayerColor;
+                                    }
+                                    else
+                                    {
+                                        Console.BackgroundColor = ComColor;
+                                    }
+                                }
+                            }
+                            ConsoleColor SavedColor = Console.BackgroundColor;
+                            Console.Write("  ");
+                            if (Curr.HasFlag(TileType.HasChecker))
+                            {
+                                if (Curr.HasFlag(TileType.Player))
+                                {
+                                    Console.BackgroundColor = PlayerColor;
+                                }
+                                else
+                                {
+                                    Console.BackgroundColor = ComColor;
+                                }
+                                if (Curr.HasFlag(TileType.King) && Middle)
+                                {
+                                    Console.BackgroundColor = KingColor;
+                                }
+                            }
+                            Console.Write("    ");
+                            Console.BackgroundColor = SavedColor;
+                            Console.Write("  ");
+                            Console.BackgroundColor = TileColorOne;
+                            Console.Write("  ");
+
+
+                            #endregion
+                        }
+                    }
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                    Console.Write("  ");
+                    WorkingWriteLine();
+
+
+                }
             }
         }
         #region Bottom Border
@@ -131,8 +225,7 @@ class Program
 
     public static void Main(string[] args)
     {
-
-        short[] GameState = { 0, 0, 0, 0, 0, 0, 0, 0 };
+        ushort[] GameState = { 0b101101101101, 585, 585, 0, 0, 1755, 1755, 0b1111111111111111 };
         VisualizeGame(GameState);
         Console.ReadKey();
     }
